16e14df21eec8d895a44d63585e89650691790d7
rahul-soshte <rahul.soshte47@gmail.com>
2017-06-19 17:54:43 +0530

    Listevent.php done

--- events_json.php
+++ events_json.php
@@ -1,20 +1,22 @@
 <?php
 require 'conn.php';
-$user_email=_GET['user_email']
+$user_email=$_POST['user_email'];
+//$user_email='rahul.soshte47@gmail.com';
 
  //fetch table rows from mysql db
     $sql = "SELECT event_id,EventName from event inner join (select event_event_id from event_has_user where user_user_id=(select user_id from user where email_id ='$user_email')) t2 on event.event_id=t2.event_event_id;";
 
-    $result = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($conn));
+    $result = mysqli_query($conn, $sql); //or die("Error in Selecting " . mysqli_error($conn));
 
     //create an array
     $eventarray = array();
-    while($row =mysqli_fetch_assoc($result))
+    while($row = mysqli_fetch_array($result))
     {
-        $eventarray[] = $row;
+        array_push($eventarray,array("event_id"=>$row[0],"EventName"=>$row[1]));
     }
-
-    echo json_encode($eventarray);
+    
+    //echo json_encode($eventarray);
+ echo json_encode(array("server_response"=>$eventarray));
 
  mysqli_close($conn);
  ?>
